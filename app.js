1542d3907c09afed6855431b11c22855afc718e4
dyllionixf0xquantum-ctrl <dyllionixf0xquantum@gmail.com>
2026-09-23 15:35:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ===== Storage =====
-const STORAGE_KEY = "binder.v1";
+const STORAGE_KEY = "binder.v2";
 const COLORS = ["#7cf6ab", "#242f7c", "#8baa27", "#8e1010", "#623c7a", "#5C7A4B","#ff0800","#00d9ff","#1fff02","#ff048e", "#6c4095"];
 
 function loadData() {
